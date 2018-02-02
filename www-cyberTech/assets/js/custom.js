a5fa6507773c439146837acd113d729fbbf8b3aa
Crypto baba <svd2305@gmail.com>
2018-02-02 13:30:34 +0530

    Website added, contract updated@

---
+++ www-cyberTech/assets/js/custom.js
@@ -0,0 +1,73 @@
+jQuery(document).ready(function() {
+    // for hover dropdown menu
+    $('ul.nav li.dropdown').hover(function() {
+        $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn(200);
+    }, function() {
+        $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(200);
+    });
+    /* slick slider call */
+    $('.slick_slider').slick({
+        dots: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        responsive: [{
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+            }
+        }, {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+        }, {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }]
+    });
+    //Check to see if the window is top if not then display button
+    jQuery(window).scroll(function() {
+        if ($(this).scrollTop() > 300) {
+            $('.scrollToTop').fadeIn();
+        } else {
+            $('.scrollToTop').fadeOut();
+        }
+    });
+    //Click event to scroll to top
+    $('.scrollToTop').click(function() {
+        $('html, body').animate({
+            scrollTop: 0
+        }, 800);
+        return false;
+    });
+    $(".search_icon").click(function() {
+        $('.search_bar').fadeOut();
+        $("i", this).toggleClass("fa-search fa-close");
+    });
+    $(document).on('click', '.fa-close', function() {
+        $('.search_bar').fadeIn();
+    });
+});
+
+wow = new WOW({
+    animateClass: 'animated',
+    offset: 100
+});
+wow.init();
+
+jQuery(window).load(function() { // makes sure the whole site is loaded
+    $('#status').fadeOut(); // will first fade out the loading animation
+    $('#preloader').delay(100).fadeOut('slow'); // will fade out the white DIV that covers the website.
+    $('body').delay(100).css({
+        'overflow': 'visible'
+    });
+})
